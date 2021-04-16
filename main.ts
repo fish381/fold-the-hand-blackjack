cf01b96245992f5eb7a2d7037d2fc37cc54f41f4
fish_381 <79244742+fish381@users.noreply.github.com>
2021-04-16 11:27:03 +1000

    more logic

--- main.ts
+++ main.ts
@@ -5,12 +5,17 @@ enum RadioMessage {
 }
 radio.onReceivedMessage(RadioMessage.findgame, function () {
     radio.sendValue("player", player)
+    my_num = player
 })
 radio.onReceivedMessage(RadioMessage.gamestart, function () {
     hand += randint(0, 13)
     hand += randint(0, 13)
     if (hand > 21) {
-    	
+        basic.showIcon(IconNames.Ghost)
+        basic.showString("you had a hand of")
+        basic.showNumber(hand)
+    } else {
+        basic.showNumber(hand)
     }
 })
 input.onButtonPressed(Button.AB, function () {
@@ -18,17 +23,18 @@ input.onButtonPressed(Button.AB, function () {
     radio.sendValue("host", 2)
     basic.pause(5000)
 })
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
 input.onGesture(Gesture.Shake, function () {
     radio.sendMessage(RadioMessage.gamestart)
 })
 radio.onReceivedValue(function (name, value) {
     player = value + 1
 })
+let my_num = 0
 let player = 0
 let hand = 0
 let game2 = 0
 hand = 0
 radio.setGroup(10)
-basic.forever(function () {
-	
-})
